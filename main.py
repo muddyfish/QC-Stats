ffb9d2643ec96412039ce5bfd13b8fd3fcc430b2
Simon Beal <sb48@sanger.ac.uk>
2015-08-18 16:30:59 +0100

    Add detection for .cram and .bam files. Detection also has a preference option, "BAM" by default

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-import csv, json
+import csv, subprocess
 
 class Main(object):
     def __init__(self, csv_file = "RunLanePlexByProject.csv"):
-        rows = self.load_csv(csv_file)
-        for row in rows:
-            rows[row]["IRODS_PATH"] = "/seq/%(RUN_ID)s/%(RUN_ID)s_%(LANE)s.bam"%rows[row]
-        print rows
+        self.samples = self.load_csv(csv_file)
+        for sample in self.samples:
+            self.samples[sample]["IRODS_BAM_PATH"] = "/seq/%(RUN_ID)s/%(RUN_ID)s_%(LANE)s.bam"%self.samples[sample]
+            self.samples[sample]["IRODS_CRAM_PATH"] = "/seq/%(RUN_ID)s/%(RUN_ID)s_%(LANE)s.cram"%self.samples[sample]
+            self.samples[sample]["IRODS_PATH"] = self.get_irods_path(sample)
+            print self.samples[sample]["IRODS_PATH"]
+        #print self.samples
+
+    def get_irods_path(self, sample, prefer = "BAM"):
+        cmd = "ils %s"%(self.samples[sample]["IRODS_%s_PATH"%prefer])
+        sub = subprocess.Popen(cmd, shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+        if sub.communicate()[0] #Was there any standard output?
+            return self.samples[sample]["IRODS_%s_PATH"%prefer]
+        return self.samples[sample]["IRODS_%s_PATH"%{"BAM":"CRAM", "CRAM":"BAM"}[prefer]]
 
     def load_csv(self, csv_filename):
         csv_file = open(csv_filename)
